652e58faa24587358bcd5b467680dc2553936ecb
Shimon <0507894317a@gmail.com>
2026-01-01 11:52:09 +0200

    dto ואנטיטי

--- shopping_server/src/users/entities/user.entity.ts
+++ shopping_server/src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+import { IsEmail,Min } from 'class-validator';
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    @IsEmail()
+    email: string;
+
+
+    @Column()
+    @Min(8)
+    password: string;
+}
+
