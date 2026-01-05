401b51c75d21730b428b73bf95ac0ddf44677cb9
Shimon <0507894317a@gmail.com>
2026-01-05 09:36:51 +0200

    יןם  חדש

--- shopping_server/src/auth/auth.module.ts
+++ shopping_server/src/auth/auth.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './dto/login.dto';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import {  AuthService  } from './auth.service';
-
+import { ConfigModule,ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [PassportModule,
-        JwtModule.register({
-            secret: 'YOUR_SECRET_KEY',
-            signOptions: { expiresIn: '1d' },
+        ConfigModule.forRoot(),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+               secret: configService.get('JWT_ENCRYPTION_KEY'),
+               signOptions: { 
+                expiresIn: configService.get('JWT_EXPIRES_IN'), 
+          },
         }),
+      }),
     ],
+
     providers: [AuthService,JwtStrategy],
     exports: [AuthService],
 })
--- shopping_server/src/auth/auth.service.ts
+++ shopping_server/src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly jwtService: JwtService) {}
+
+    async validateToken(token: string): Promise<any> {
+        try {
+            const decoded = this.jwtService.verify(token);
+            return decoded;
+        } catch (error) {
+            throw new UnauthorizedException('Invalid token');
+        }
+    }
+
+    generateToken(payload: any): string {
+        return this.jwtService.sign(payload);
+    }
+}
