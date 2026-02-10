f5804cc5ec0c1a1512a52897b37bd4635ddf8a06
Shimon <0507894317a@example.com>
2026-02-10 11:12:52 +0200

    סידור ההתחברות עם גוגל

--- shopping_server/src/auth/strategies/google.strategy.ts
+++ shopping_server/src/auth/strategies/google.strategy.ts
@@ -12,7 +12,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
             clientSecret: configService.getOrThrow<string>('GOOGLE_CLIENT_SECRET'),
             callbackURL: configService.getOrThrow<string>('GOOGLE_CALLBACK_URL'),
             scope: ['email', 'profile'],
-        });
+            prompt: 'select_account',
+        } as any);
     }
 
     async validate(
